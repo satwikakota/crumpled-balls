1d7d6b9a1611c374e5cfca1d4c6970d481d48cdf
satwikakota <satwika.kota@gmail.com>
2020-09-06 21:31:24 -0400

    Initial Commit

--- sketch.js
+++ sketch.js
@@ -3,8 +3,10 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-
-
+var dustbinimg;
+function preload(){
+	dustbinimg=loadImage("Sprites/dustbingreen.png");
+}
 function setup() {
 	createCanvas(800, 700);
 
@@ -16,8 +18,7 @@ function setup() {
 	paper1= new Paper(100,630,70); 
 	dustbin1= new Dustbin(600,650,20,400); 
 	dusbin2= new Dustbin(500,700,200,20); 
-	dustbin3= new Dustbin(400,650,20,400); 
-	dustbinIMG= new DustbinIMG(500,700,200,300); 
+	dustbin3= new Dustbin(400,650,20,400);  
   }
 
 
@@ -30,9 +31,9 @@ function draw() {
   dustbin1.display();
   dusbin2.display();
   dustbin3.display();
-  dustbinIMG.display(); 
+  imageMode(CENTER);
+  image(dustbinimg,500,550,250,300);
   keyPressed(); 
-  drawSprites();
  
 }
 function keyPressed(){
